112344123d702ed35236e4dcc211be93597a4a6b
Anuj Gill <anujgill212@gmail.com>
2023-11-01 18:25:29 +0530

    Lecture 2 done

--- flask_blog/flaskBlog.py
+++ flask_blog/flaskBlog.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        
+        'author' : 'Sahil Shangloo',
+        'title' : 'Release of My New Song',
+        'date' : '01/11/2023',
+        'content' : "Hello everyone, this is Sahil Shangloo from Jammu. I am happy to announce the release of my new Rap before you. The song will be availabel on Spotify from 3rd of November!"
+    },
+    {
+        
+        'author' : 'Om Alve',
+        'title' : 'Neural Network',
+        'date' : '28/10/2023',
+        'content' : "Hello everyone, this is Om Alve from Thane. I am happy to announce the release of my new Rap before you. The song will be availabel on Spotify from 3rd of November!"
+    }
+
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",posts = posts,title = "Welcome")
 
 @app.route("/about")
 def about():
