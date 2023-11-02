8705dd2449ad5edbf76a77bae3c451af7c53eb9a
Anuj Gill <anujgill212@gmail.com>
2023-11-02 20:47:01 +0530

    Day 2

--- flask_blog/flaskBlog.py
+++ flask_blog/flaskBlog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm,LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8d9a018d01741fc317ce5e4050ee3740'
 
 posts = [
     {
@@ -28,6 +31,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',form=form,title='register')
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',form=form,title='Login')
 
 if __name__ == "__main__":
     app.run(debug=True)
