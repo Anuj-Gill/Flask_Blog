641f60a767dc15b65f21dc014f7c68db5157646b
Anuj Gill <anujgill212@gmail.com>
2023-11-05 22:17:08 +0530

    Lecture 3

--- flask_blog/flaskBlog.py
+++ flask_blog/flaskBlog.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,flash, redirect
 from forms import RegistrationForm,LoginForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '8d9a018d01741fc317ce5e4050ee3740'
+app.config['SECRET_KEY'] = '8d9a018d01741fc317ce5e4050ee3740' #generated using secret module
 
 posts = [
     {
@@ -32,14 +32,25 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST']) #methods is used to specify that this specific route can accept these methods
 def register():
     form = RegistrationForm()
-    return render_template('register.html',form=form,title='register')
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    # else:
+    #     flash
+    return render_template('register.html',form=form,title='Register')
 
-@app.route("/login")
+@app.route("/login",methods=['POST','GET'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'gillanuj1208@gmail.com' and form.password.data == 'password':
+            flash(f'Logged In Successfully!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unseccessful. Please check username and password','danger')
     return render_template('login.html',form=form,title='Login')
 
 if __name__ == "__main__":
--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,13 +1,15 @@
+#for creating forms we need many different validation checks, some reg-ex for valid email, check password etc. For this is an extension wtforms which makes all these things easy.We will be writing python classes representing forms, they will be converted to html forms in templates
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
-class RegistrationForm(FlaskForm):
-  username = StringField('Username',validators=[DataRequired(),Length(min = 2,max = 20)])
+class RegistrationForm(FlaskForm): # use of inheritance, RegistratonForm(child class) inheriting from FlaskForm(parent class)  
+  username = StringField('Username',validators=[DataRequired(),Length(min = 2,max = 20)]) #first argument is the name of the field and it will also be used as a label in out html. 
   email = StringField('Email', validators=[DataRequired(),Email()])
   password = PasswordField("Password",validators=[DataRequired()])
   confirm_password = PasswordField("Password",validators=[DataRequired(),EqualTo('password')])
-  submit = SubmitField('Sign Up')
+  submit = SubmitField('Sign Up') #Sign up is the label
 
 
 class LoginForm(FlaskForm):
