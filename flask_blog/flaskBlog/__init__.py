161fa60b2427f33c0fb1ed30a61c21b5cb8320a7
Anuj Gill <anujgill212@gmail.com>
2023-11-16 11:29:02 +0530

    Disabled CSRF protection due to hosting error

--- flask_blog/flaskBlog/__init__.py
+++ flask_blog/flaskBlog/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os 
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '8d9a018d01741fc317ce5e4050ee3740' #generated using secret module
+load_dotenv("../.env")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY") #generated using secret module
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #/// is for the relative paths, means that site.db will be created in the same file where py file is there
+app.config["WTF_CSRF_ENABLED"] = False
 db = SQLAlchemy(app) #instance of sqlalchemy
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
